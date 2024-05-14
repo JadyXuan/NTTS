2434cec6baebf81c3a6c78b11b455d3d65f00677
Shixian Li <34830785+znsoooo@users.noreply.github.com>
2024-05-15 06:15:27 +0800

    Fix display errors
    - error should be print out from "sys.stderr"
    - "op.path" for showing correct path in Windows
    - remove unused variables

--- ntts.py
+++ ntts.py
@@ -1,27 +1,24 @@
+import os
 import sys
 import traceback
 import importlib.util
 import site
 
 def excepthook_decorator(excepthook):
-    def wrapper(*args, **kwargs):
-        exctype, value, exctracback = args
+    def wrapper(exctype, value, exctracback):
         # print(exctype, value, exctracback)
         # package_path = importlib.util.find_spec('numpy').origin
-        package_path = site.getsitepackages()[1].replace('\\', '/')
-        if exctype == KeyboardInterrupt:
+        package_path = site.getsitepackages()[1]
+        if exctype is KeyboardInterrupt:
             format = traceback.format_tb(exctracback)
-            frame = {'filename': str(package_path)+'/mindx/model.py', 'lineno':23, 'name': format[-1].split('\n')[0].split(' ')[-1], 'line': 'Model.inference(img)', 'locals': None, 'exname': 'KeyboardInterrupt'}
+            frame = {'filename': os.path.join(package_path, 'mindx', 'model.py'), 'lineno': 23, 'name': format[-1].split('\n')[0].split(' ')[-1], 'line': 'Model.inference(img)', 'locals': None, 'exname': 'KeyboardInterrupt'}
             msg = ''.join(reformat(frame, format))
-            print(msg)
+            print(msg, file=sys.stderr)
         else:
             excepthook(exctype, value, exctracback)
     return wrapper
 
 def reformat(frame, format):
-    result = []
-    count = 0
-    count += 1
     format[0] = 'Traceback (most recent call last):\n' + format[0]
     row = []
     row.append('  File "{}", line {}, in {}\n'.format(
